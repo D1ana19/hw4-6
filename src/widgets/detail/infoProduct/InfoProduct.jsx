7697743e1e3625f529dd60be4a55875a08a792ca
D1ana19 <aleksandrovnadiana25@gmail.com>
2024-12-27 19:30:54 +0600

    first commit

---
+++ src/widgets/detail/infoProduct/InfoProduct.jsx
@@ -0,0 +1,28 @@
+import ReactStars from 'react-stars';
+import './infoProduct.css'
+
+export const InfoProduct = ({product, addCart}) => {
+    return (
+        <div>
+            <div className="productInfo">
+                <h2>{product?.title}</h2>
+                <div className="rating">
+                    <ReactStars
+                        count={5}
+                        value={4.5}  
+                        isHalf={true}          
+                        size={30}       
+                        edit={false}    
+                        activeColor="#ffd700" 
+                        inactiveColor="#ffffff"
+                    />
+                    <p>4.5/5</p>
+                </div>
+                <h3>${product?.price}</h3>
+                <p>{product?.description}</p>
+                <button onClick={()=> addCart(product)} className="Button" >Buy</button>
+            </div>
+        </div>
+    );
+}
+
---
+++ src/widgets/detail/photoSection/PhotoSection.jsx
@@ -0,0 +1,31 @@
+import './photoSection.css';
+
+export const PhotoSection = ({ product }) => {
+    
+    return (
+        <div className='photoCont'>
+            {
+                product && 
+                (
+                    <div className='photoFlex'>
+                        <div className='images'>
+                            <div>
+                                <img src={product?.category.image} alt="" />     
+                            </div>
+                            <div>
+                                <img src={product?.category.image} alt="" />     
+                            </div>
+                            <div>
+                                <img src={product?.category.image} alt="" />     
+                            </div>   
+                        </div>
+                        <div className='mainImg'>
+                            <img src={product?.category.image} alt="" />     
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
+
